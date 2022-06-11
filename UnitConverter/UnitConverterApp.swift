9aa732b4d5d94fec3c563d36055be62ff164af08
Fawzi <fawzialrifai@icloud.com>
2022-06-11 16:05:07 +0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Fawzi Rifai on 11/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
